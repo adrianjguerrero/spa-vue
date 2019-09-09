e8ad34b1da5f33a994e21bf407cf7e15c390e5ca
Adrian Guerrero <adrianjguerrero87@gmail.com>
2019-09-08 22:51:45 -0400

    protocol http

--- src/api/index.js
+++ src/api/index.js
@@ -2,7 +2,7 @@ import config from './config';
 
 
 const API_KEY = config.apiKey;
-const URL =  country => `http://ws.audioscrobbler.com/2.0/?method=geo.gettopartists&country=${country}&api_key=${API_KEY}&format=json`;
+const URL =  country => `https://ws.audioscrobbler.com/2.0/?method=geo.gettopartists&country=${country}&api_key=${API_KEY}&format=json`;
 
 export default function getArtists(country) {
     return fetch(URL(country))
